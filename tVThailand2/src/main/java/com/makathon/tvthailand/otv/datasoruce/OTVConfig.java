717aabe07147a1d0e6365c3d90ddd19c256b4d2f
Nattapong Tonprasert <luciferultram@gmail.com>
2014-07-21 17:01:42 +0700

    Change API Version to 2.0.1.2

--- tVThailand2/src/main/java/com/makathon/tvthailand/otv/datasoruce/OTVConfig.java
+++ tVThailand2/src/main/java/com/makathon/tvthailand/otv/datasoruce/OTVConfig.java
@@ -3,5 +3,5 @@ package com.makathon.tvthailand.otv.datasoruce;
 public class OTVConfig {
 	public static final String BASE_URL = "http://api.otv.co.th/api/index.php";
 	public static final String APP_ID = "15";
-	public static final String API_VERSION = "1.0";
+	public static final String API_VERSION = "2.0.1.2";
 }
