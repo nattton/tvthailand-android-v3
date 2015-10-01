0b94c6286012f96a3fcf9817bb07b748b80f06a2
Nattapong Tonprasert <luciferultram@gmail.com>
2015-10-01 16:14:50 +0700

    refactor warning

--- tVThailand2/src/main/java/com/codemobi/android/tvthailand/datasource/OnLoadListener.java
+++
@@ -1,9 +0,0 @@
-package com.codemobi.android.tvthailand.datasource;
-
-/**
- * Created by nattapong on 9/29/15 AD.
- */
-public interface OnLoadListener {
-    void onLoadStart();
-    void onLoadFinished();
-}
